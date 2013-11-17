88787e9cc76c5021227d4af67a0fbef8aac1dcb5
Simon <siimon@ubuntu.ubuntu-domain>
2013-11-17 02:26:25 -0500

    Worked on pFinder more. Needs to be split into arrays now and put into db

--- pFinder.py
+++ pFinder.py
@@ -2,10 +2,16 @@ import urllib
 from bs4 import BeautifulSoup
 
 #Started working on it
+#created a pFinder 
+#returns the part with address now
 
-url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-Mike%20-Zamansky/")
+url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-Kevin-Li")
 result = BeautifulSoup(url)
-l = []
+result = result.find_all('div', attrs={'class' : 'ticklerResultsDatum ticklerResultsColAddr datumAddr'})
+
+def pFind(first,last):
+    url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-%s-%s/0/", first,last).find("li")
+    result = BeautifulSoup(url)
 
 
 print(result);
