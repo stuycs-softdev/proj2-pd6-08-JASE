5e06575da6f0824a71f8fe52f773b661f902dbf5
Simon <siimon@ubuntu.ubuntu-domain>
2013-11-17 18:05:21 -0500

    Added a general code. Bugs : Need to add a ' ' after 'Ave'

--- pFinder.py
+++ pFinder.py
@@ -1,40 +1,40 @@
 import urllib
 from bs4 import BeautifulSoup
 
-#Started working on it
-#created a pFinder 
-#returns the part with address now
-
-url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-Kevin-Li")
-result = BeautifulSoup(url)
-data = []
-for x in result.find_all('div', attrs={'class' : 'ticklerResultsDatum ticklerResultsColAddr datumAddr'}):
-    phoneNum = x.find('span')
-    x.find('span').replaceWith(' ');
-    if(phoneNum):
-        phoneNum = phoneNum.get_text()   
-    else:
-        phoneNum = 'UnListed PhoneNumber'
-    address = x.get_text()
-    data.append({"address":address,
-                 "phoneNum":phoneNum})
-
-
-def pFind(first,last):
-    url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-%s-%s/0/", first,last).find("li")
+def test():
+    url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-Kevin-Li")
     result = BeautifulSoup(url)
     data = []
     for x in result.find_all('div', attrs={'class' : 'ticklerResultsDatum ticklerResultsColAddr datumAddr'}):
         phoneNum = x.find('span')
         x.find('span').replaceWith(' ');
         if(phoneNum):
+            phoneNum = phoneNum.get_text()   
+        else:
+            phoneNum = 'UnListed PhoneNumber'
+        address = x.get_text()
+        data.append({"address":address,
+                     "phoneNum":phoneNum})
+    return data
+
+
+
+def pFind(first,last):
+    url = urllib.urlopen("http://www.peoplefinder.com/people-search/NY-"+ first + "-" +last)
+    result = BeautifulSoup(url)
+    data = []
+    for x in result.find_all('div', attrs={'class' : 'ticklerResultsDatum ticklerResultsColAddr datumAddr'}):
+        print(data)
+        phoneNum = x.find('span')
+        x.find('span').replaceWith(' ');
+        if(phoneNum):
             phoneNum = phoneNum.get_text()
         else:
             phoneNum = 'UnListed PhoneNumber'
-            address = x.get_text()
-            data.append({"address":address,
-                         "phoneNum":phoneNum})
+        address = x.get_text()
+        data.append({"address":address,
+                 "phoneNum":phoneNum})
+    return data
 
+print(pFind('Kevin','Li'))
 
-
-print(data[0])
