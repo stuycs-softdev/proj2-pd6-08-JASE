5ebe35e2f0638dde2d3d36db173dd9223430aa65
EliCohen96 <elic90@gmail.com>
2013-11-16 22:03:45 -0500

    now returns name, overall rating, and subject of all teachers on RMT

--- ratemt.py
+++ ratemt.py
@@ -3,22 +3,36 @@ from bs4 import BeautifulSoup
 from operator import itemgetter
 
 def getRatings():
-    
-    url = "http://www.ratemyteachers.com/stuyvesant-high-school/13765-s"
+    results = getPageRatings("")
+    x = 2
+    prev = []
+    while True:
+        temp = getPageRatings("/%d" %(x))
+        if prev == temp:
+            break
+        results += temp
+        prev = temp
+        x += 1
+    return results
+
+def getPageRatings(page_num):
+    results = []
+
+    url = "http://www.ratemyteachers.com/stuyvesant-high-school/13765-s" + page_num
 
     page = BeautifulSoup(urllib.urlopen(url))
 
-    results = []
-
     for x in page.find("table").find_all("tr"):
         if len(x.find_all("td")) > 0:
-            name = x.find_all("td")[0].find("span").get_text()
+            a = x.find_all("td")
+            name = a[0].find("span").get_text()
+            overall = a[1].find("span").get_text()
+            subject = a[2].get_text()
         
-            print(name)
-           
+            results.append({"name":name, "overall":overall, "subject":subject})
 
-    return page
+    return results
 
 
 if __name__ == "__main__":
-    getRatings()
+   print(getRatings())
