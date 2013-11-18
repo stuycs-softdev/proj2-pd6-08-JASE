aed1cf215574d5a168825675f7021eea26bea7e3
RatedRion <ratedrion@gmail.com>
2013-11-18 05:27:41 -0500

    update test

--- gimages.py
+++ gimages.py
@@ -3,10 +3,10 @@ import urllib
 def gImages(teacher):
     url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + teacher
     data = []
-	for x in url.find('unescapedUrl'):
-		image = x.get_text()
-		data.append({"image":image})
-	return data
+    for x in url.find('unescapedUrl'):
+	image = x.get_text()
+	data.append({"image":image})
+    return data
 
 
 if __name__ == "__main__":
