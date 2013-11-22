14ada9731336d2b9529ed0eebfec3bbe38b9c8cf
Andrew-Zarenberg <andzz42@gmail.com>
2013-11-21 22:49:03 -0500

    When adding to the database, program will now print out results as they're entered in

--- stuyteachers.py
+++ stuyteachers.py
@@ -70,6 +70,23 @@ def teachersToDatabase():
 
         c.teachers.Collections.insert(teachers[x])
 
+#        if t["salary"] == -1:
+#            print("[%d] Error  %s %s"%(x,t["first"],t["last"]))
+#        else:
+        pr = "[%d]\t{{ %s %s }}\t"%(x,t["first"],t["last"])
+        if t["salary"] == -1:
+            pr += "Error "
+        else:
+            pr += "$%d"%(t["salary"])
+        if(len(t["address"]) > 0):
+            pr += "\t%s\t%s"%(t["address"][0]["phoneNum"],t["address"][0]["address"])
+        else:
+            pr += "No address found"
+        pr += "\n"
+        print(pr)
+#        print("[%d]\t$%s\t%s\t%s\t%s\t%s"%(x,t["salary"],t["first"],t["last"],t["address"][0]["phoneNum"],t["address"][0]["address"]))
+
+
 
 if __name__ == "__main__":
     teachersToDatabase()
