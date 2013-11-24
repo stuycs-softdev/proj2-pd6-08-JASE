618147735d2d5b4c213e2d4c69df233b693ad761
RatedRion <ratedrion@gmail.com>
2013-11-24 16:27:01 -0500

    slow update

--- gmap.py
+++ gmap.py
@@ -1,20 +1,17 @@
 from flask import Flask
-from flask import request
-from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def gmap(addr,zipc):
+def gmap(addr):
     c = MongoClient()
-    addr = "maps.googleapis.com/maps/api/staticmap?center=" + addr + " " + zip + "&markers=color:blue%7Clabel:C%7C" + addr + " " + zip + "&zoom=16&size=640x640&sensor=false"
-    for x in range(0,len(teachers)):
+    addr = "maps.googleapis.com/maps/api/staticmap?center=" + addr + "&markers=color:blue%7Clabel:C%7C" + addr + " " + zip + "&zoom=16&size=640x640&sensor=false&key=AIzaSyAEG1zvTqgnTHLX8WIerdMnjZiB2Scyys0"
+    for x in range(0,len(tea):
         teachers[x]["address"] = x
         c.teachers.Collections.insert(addr[x])
 
 #maps.googleapis.com/maps/api/staticmap?center={{addr}}&markers=color:blue%7Clabel:C%7C{{addr}}&zoom=16&size=640x640&sensor=false
 if __name__ == "__main__":
     addr = raw_input("Enter address: ")
-    zipc = raw_input("Enter zip code: ")
-    data = gmap(addr,zipc)
-    print("maps.googleapis.com/maps/api/staticmap?center=" + addr + " " + zip + "&markers=color:blue%7Clabel:C%7C" + addr + " " + zip + "&zoom=16&size=640x640&sensor=false")
+    data = gmap(addr);
+    
