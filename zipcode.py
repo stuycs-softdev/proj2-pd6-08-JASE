07a8122dfea0394427e2019c3eb7d46178879c04
EliCohen96 <elic90@gmail.com>
2013-11-26 00:09:36 -0500

    now returns a giant string of data

--- zipcode.py
+++ zipcode.py
@@ -1,10 +1,14 @@
 import urllib
 from bs4 import BeautifulSoup
 from operator import itemgetter
+import sys
 
-zipinfo(zipcode){
-    pass
-}
+def zipinfo(zipcode):
+    url = "http://zipwho.com/?zip=" + zipcode  + "&city=&filters=--_--_--_--&state=&mode=zip"
+    page = BeautifulSoup(urllib.urlopen(url))
+    x = page.find("script").get_text()
+    return x
+
 
 if __name__ == "__main__":
-    pass
+    print(zipinfo(sys.argv[1]))
