c94d2400da607a766c719d73e529de87c6ce7267
EliCohen96 <elic90@gmail.com>
2013-11-29 16:04:23 -0500

    scraping  bug is fixed

--- zipcode.py
+++ zipcode.py
@@ -1,14 +1,22 @@
 import urllib
+import urllib2
 from bs4 import BeautifulSoup
 from operator import itemgetter
 import sys
 
 def zipinfo(zipcode):
+
     url = "http://zipwho.com/?zip=" + zipcode  + "&city=&filters=--_--_--_--&state=&mode=zip"
-    page = BeautifulSoup(urllib.urlopen(url))
-    x = page.find("script").get_text()
-    return x.split('return "')[1].split('";')[0]
+
+    request = urllib2.Request(url, headers={'User-Agent':"Magic Browser"})
+    result = urllib2.urlopen(request)
+
+    page = BeautifulSoup(result)
+
+    x = page.find("script").get_text().split("\"")[1].split(",");
+    return x
+
 
 
 if __name__ == "__main__":
-    print(zipinfo("10075"))
+    print(zipinfo("1011215"))
