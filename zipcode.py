a80821f7b4d57e89b1b5e91e028ae918562ea43e
EliCohen96 <elic90@gmail.com>
2013-11-29 16:24:23 -0500

    data is now organized into dictionary

--- zipcode.py
+++ zipcode.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from operator import itemgetter
 import sys
 
+
+
 def zipinfo(zipcode):
 
     url = "http://zipwho.com/?zip=" + zipcode  + "&city=&filters=--_--_--_--&state=&mode=zip"
@@ -12,9 +14,22 @@ def zipinfo(zipcode):
     result = urllib2.urlopen(request)
 
     page = BeautifulSoup(result)
+    
+    x = page.find("script").get_text().split("\"")[1].replace('\\n',',').split(',')
 
-    x = page.find("script").get_text().split("\"")[1].split(",");
-    return x
+    namenum = 0
+    statnum = len(x)/2
+    
+    data = {}
+
+    while(statnum < len(x)):
+        data[x[namenum]] = x[statnum]
+        namenum += 1
+        statnum += 1
+        
+
+    return data
+
 
 
 
